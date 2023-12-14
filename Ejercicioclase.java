9c0e40e9a313df77404f09af5f474eaad930b755
Angieal56 <analvarado1@espe.edu.ec>
2023-12-14 00:23:45 -0500

    CODIGOS 02_02

---
+++ Ejercicioclase.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.persona;
+
+/**
+ *
+ * @author kelly
+ */
+public class Ejercicioclase {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public int id;
+
+    public Ejercicioclase() {
+    }
+
+    public Ejercicioclase(String nombre, String apellido, int edad, int id) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    void setId(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ Persona.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persona;
+
+/**
+ *
+ * @author kelly
+ */
+public class Persona {
+
+    public static void main(String[] args) {
+        Ejercicioclase persona1 = new Ejercicioclase();
+        persona1.setId("1726955972");
+
+        System.out.println(persona1.getId());
+    }
+}
